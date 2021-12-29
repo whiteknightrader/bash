a127a97f70ec18b9c01c8916628a4196f2bd62f4
whiteknightrader <46353849+whiteknightrader@users.noreply.github.com>
2021-12-29 11:58:49 +0530

    Create ubuntu_gui_remote.sh

---
+++ ubuntu_gui_remote.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+adduser wolf
+addgroup wolf sudo
+
+ufw enable
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow ssh
+ufw allow 5901
+
+sudo apt-get install -y xfce4 xfce4-goodies tightvncserver firefox expect git
+su - wolf
+vncserver
+
+vncserver -kill :1
+cp ~/.vnc/xstartup ~/.vnc/xstartup.bak
+
+sudo echo "startxfce4 &" >> ~/.vnc/xstartup
+
+sudo cat > /etc/systemd/system/vncserver@.service << EOF
+[Unit]
+ Description=Remote desktop service (VNC)
+ After=syslog.target network.target
+
+[Service]
+  Type=forking
+  User=wolf
+  PIDFile=/home/wolf/.vnc/%H:%i.pid
+  ExecStartPre=-/usr/bin/vncserver -kill :%i > /dev/null 2>&1
+  ExecStart=/usr/bin/vncserver -depth 24 -geometry 1280x800 :%i
+  ExecStop=/usr/bin/vncserver -kill :%i
+
+[Install]
+  WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable --now vncserver@1.service
+
+git clone https://github.com/whiteknightrader/bash.git && chmod +x ./bash/*.sh
+
+sudo reboot
